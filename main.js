fe01ca3445158968db7918a0b82d8f57869cd8e2
hodamansour <hodamansour164@gmail.com>
2019-03-27 10:16:02 +0200

    searching a correct api, and solving our problem

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-document.getElementById('button').addEventListener("click", getMediaWikiData);
-var url = 'https://www.mediawiki.org/w/api.php?action=opensearch&search=';
-
-function getMediaWikiData (event) {
-var keyword = document.getElementById('input').value;
-var searchURL = url + keyword;
-fetch(searchURL)
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(data) {
-    console.log(data);
-  })
-  .catch(function(error) {
-    console.log(error);
-  })
+var url = 'https://www.mediawiki.org/w/api.php?action=opensearch&format=json&origin=*&search=';
+// var url = 'https://www.mediawiki.org/w/api.php?action=parse&format=json'
+function getMediaWikiData(event) {
+  var keyword = document.getElementById('input').value;
+  var searchURL = url + keyword;
+  console.log(searchURL);
+  fetch(searchURL)
+    .then(function(response) {
+      // console.log(response.text());
+      return response.json();
+    })
+    .then(function(data) {
+      console.log(data);
+      // console.log(data ? JSON.parse(data) : {});
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
 }
+
+document.getElementById('button').addEventListener("click", getMediaWikiData);
